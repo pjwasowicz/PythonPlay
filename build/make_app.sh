82e5c081573974713741d5132c2ed1c2c30cb662
pjwasowicz <pjwasowicz@gmail.com>
2025-01-03 19:12:16 +0100

    build dla macos

--- build/make_app.sh
+++ build/make_app.sh
@@ -1,4 +1,6 @@
 rm -r ./milonga_dist.app/Contents/MacOS/
 mkdir ./milonga_dist.app/Contents/MacOS/
 cp -R exe.macosx-10.13-universal2-3.12/* ./milonga_dist.app/Contents/MacOS
+rm -r milonga.app
 cp -R milonga_dist.app milonga.app
+hdiutil create -volname "Milonga" -srcfolder "milonga.app" -ov -format UDZO "Milonga.dmg"
